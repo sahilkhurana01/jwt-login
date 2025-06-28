5a3eee998b71dae39e8656b33be30b7f6f1e967c
sahilkhurana01 <khuranasahil099@gmail.com>
2025-06-28 15:48:42 +0530

    env changed

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: [ 'http://localhost:5173', 'https://jwt-login-dmh5figi0-sahil-arts-projects.vercel.app' ],
     credentials: true
 }))
 
